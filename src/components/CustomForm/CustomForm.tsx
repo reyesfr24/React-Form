34e2adb105af714a3abe0fa7b532e1d8abb07476
reyesfr24 <franciscoreyesluna@gmail.com>
2025-07-23 21:41:24 +0200

    doc: Añade comentarios a 'CustomForm' y 'CustomInput'

--- src/components/CustomForm/CustomForm.tsx
+++ src/components/CustomForm/CustomForm.tsx
@@ -1,12 +1,24 @@
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"; // Librería de validación: adapta el esquema de validación de Zod para usarlo dentro de React Hook Form.
 import { type SubmitHandler, useForm } from "react-hook-form"
 import InputForm from "./components/CustomInput"
-import { type FormValues, schema } from "./models" 
+import { type FormValues, schema } from "./models" // tipo inferido de Zod (para TypeScript) y el esquema de validación que define las reglas del formulario.
+
+// useForm() crea un centro de control del formulario.
+// El control que devuelve se pasa a los componentes hijos.
+// Controller conecta cada input con ese sistema de control.
+// Así puedes usar inputs personalizados sin perder validación, estado ni errores.
 
 const CustomForm = () => {
   const {control, handleSubmit, formState: { errors }} = 
-  useForm<FormValues>({
-    resolver: zodResolver(schema),
+  useForm<FormValues>({ // El tipo de los datos del formulario se establecen en FormValues que a su vez se infiere del esquema zod.
+    // Como en los tipos hay "name: string, email:string.." en el formulario tendrá que haber un elemento con un atributo name, email.. y todos los que haya.
+    
+    // Control es necesario para conectar el formulario con los inputs personalizados. Siempre se suele llamar así
+    // handleSubmit es una función envuelve al onSubmit del formulario para que solo se envíe datos válidos.
+    // formState: { errors }, // Aquí se almacenan los errores de validación del formulario 
+
+    resolver: zodResolver(schema), // conectamos la validación de Zod con React Hook Form. Si el formulario no cumple las reglas del schema, se generan errores automáticamente.
+    // Si no estuviera resolver tendíramos que validar manualmente el cada input
     defaultValues: {
       name: '',
       email: '',
@@ -15,7 +27,8 @@ const CustomForm = () => {
     } 
   });
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => { // declara un función de tipo SubmitHandler (proporcionada por React Hook Form) y le especifica
+    // que el tipo de datos que recibe es FormValues.
     console.log(data)
   }
 
@@ -26,6 +39,10 @@ const CustomForm = () => {
       <InputForm name="password" control={control} label="Contraseña" type="password" error={errors.password} />
       <InputForm name="confirmPassword" control={control} label="Confirmar contraseña" type="password" error={errors.confirmPassword} />
       <button type="submit">Enviar</button>
+      {/* Control se necesita siempre que se use el componente Controller (definido en el componente InputForm) por usar
+      campo controlados (React controla el valor con su estado) */}
+
+
     </form>
   )
 }
--- src/components/CustomForm/components/CustomInput.tsx
+++ src/components/CustomForm/components/CustomInput.tsx
@@ -1,9 +1,11 @@
-import  { type FieldError, type Control, Controller }  from "react-hook-form"
+// FiledError: tipo para representar errores de un campo
+
+import  { type FieldError, type Control, Controller }  from "react-hook-form" 
 import './CustomInput.css'
 import type { FormValues } from "../models";
 
 interface Props {
-  name: keyof FormValues;
+  name: keyof FormValues; // keyof es un operador que permite obtener las claves de un tipo de objeto como un tipo literal.
   control: Control<FormValues>;
   label: string;
   type?: string;
@@ -13,14 +15,28 @@ interface Props {
 const InputForm = ({name, control, label, type, error }: Props) => {
   return (
       <div className="form-group">
-        <label htmlFor={name}>{label}</label>
+        
+        {/* Esto vincula el texto del label descriptivo con el input a través de la etiqueta htmlFor que tiene que tenerl el mismo
+        valor que el id del input */}
+        <label htmlFor={name}>{label}</label> 
+        
+        {/* Controller es un componente de React Hook Form que conecta un input personalizado 
+        (no nativo o no registrado automáticamente) con el sistema de formularios. */}
         <Controller 
-          name={name}
+          name={name} // Nombre del campo que estamos controlando. Tiene que coincidir con el nombre del campo en el esquema de validación.
           control={control} 
+
+          // {field} es un objeto que React Hook Form da automáticamente, y que contiene todas las props necesarias para 
+           //que ese input esté conectado al formulario ( incluye onChange, onBlur, value, name, ref, etc.).
           render={ ({field}) => 
             <input 
               id={name} 
-              type={type} {...field} 
+              type={type}
+              
+              // desestructuración con el spread operator es como si escribieramos una a una las propiedades de field:
+              // name={field.name}
+              // value={field.value} .....
+              {...field} 
               className={`form-control ${ error ? "is-invalid" : "" }`}
             />
           }
