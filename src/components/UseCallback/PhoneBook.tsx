b9fc1a834b102dcae8e7e0a51e93ed42f60dfbc7
reyesfr24 <franciscoreyesluna@gmail.com>
2025-08-16 12:18:50 +0200

    feat: useCallback terminado

--- src/components/UseCallback/PhoneBook.tsx
+++ src/components/UseCallback/PhoneBook.tsx
@@ -10,7 +10,7 @@
   lo que contiene), como la función será igual y es parámetro de otro componente (hijo), no se contruya la función de nuevo para que no renderice el
   componente donde es parámetro
 */
-import { useCallback, useState } from "react";
+import { memo, useCallback, useState } from "react";
 
 interface Contact {
   id: number;
@@ -23,36 +23,40 @@ interface ContactProps {
   onCall: (phone: string) => void
 }
 
-// Componente hijo
-const ContactCard = ({ contact, onCall }: ContactProps) => {
+/* 
+  Componente hijo
+  se usa memo para memorizar el componente y así conseguir que aunque se renderice todo por el cambio de estado, no renderice los hijos si no
+  cambian sus propiedades 
+*/
+const ContactCard = memo(({ contact, onCall }: ContactProps) => {
   console.log(`Renderizando contacto ${contact.name}`)
 
   return (
     <div>
       <h3>{contact.name}</h3>
       <p>Teléfono: {contact.phone}</p>
-      <button onClick={() => onCall(contact.phone)}>Llamar</button>
+      <button onClick={() => onCall(contact.name)}>Llamar</button>
     </div>
   )
-}
+})
 
 // Componente padre
 export const PhoneBook = () => {
   const [contacts, setContacts] = useState<Contact[]>([
     {
-      id: 1, name: "Manzana", phone: "123-456-789"
+      id: 1, name: "Antonio", phone: "123-456-789"
     },
     {
-      id: 2, name: "Pera", phone: "123-456-789"
+      id: 2, name: "Lucia", phone: "234-567-891"
     },
     {
-      id: 3, name: "Leche", phone: "123-456-789"
+      id: 3, name: "Jose", phone: "345-678-912"
     },
   ])
 
   const [log, setLog] = useState<string>("")
 
-  const makeCall = useCallback((phone: string) => setLog(`Llamando al ${phone}`), [] )
+  const makeCall = useCallback((name: string) => setLog(`Llamando al ${name}`), [] )
 
   const addContact = () => {
     const newContact = {
@@ -64,6 +68,11 @@ export const PhoneBook = () => {
     setContacts([...contacts, newContact])
   }
 
+  /* 
+    En este return no le pasamos la función makeCall en sí (por eso no lleva el parámetro de entrada)
+    Lo que le pasamos es la referencia a la función. EN la definición del ContactCard es donde se define
+    que la función que reciba por props se le pasará el argumento contact.phone
+  */
   return (
     <div>
       <h2>Agenda de Contacto</h2>
